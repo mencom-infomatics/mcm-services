62645ae210248417d787c0e3e9050f6b7831514c
Subham Datta <subhamdatta11@gmail.com>
2026-07-13 15:45:00 +0530

    Initialize

---
+++ mcm-admin-server/src/test/java/org/mencom/server/admin/AdminServerTests.java
@@ -0,0 +1,14 @@
+package org.mencom.server.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AdminServerTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
---
+++ mcm-eureka-server/src/test/java/org/mencom/server/eureka/EurekaServerTests.java
@@ -0,0 +1,13 @@
+package org.mencom.server.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
